1df03201543eca5f093e05700a0e6c1bf5ddf30a
Xmaster6y <yoann.poupart@ens-lyon.fr>
2023-10-23 11:55:42 +0200

    seq encode test

---
+++ tests/utils/conftest.py
@@ -0,0 +1,38 @@
+"""
+Fixtures for testing utils.
+"""
+
+import pytest
+import torch
+
+import src.utils.translate as translate
+from src.models.decision_transformer import DecisionTransformerConfig, DecisionTransformerModel
+
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+torch.set_default_device(DEVICE)
+
+
+@pytest.fixture(scope="module")
+def simple_seq():
+    return "1. d4 d5 2. c4 e6 3. e3 Nd7 4. cxd5 exd5 5. Nc3 Ngf6 6. h3 Bd6"
+
+
+@pytest.fixture(scope="module")
+def default_64_model():
+    conf = DecisionTransformerConfig(
+        state_dim=64,
+        act_dim=4672,
+    )
+    model = DecisionTransformerModel(conf)
+    yield model
+
+
+@pytest.fixture(scope="module")
+def default_64x12_model():
+    conf = DecisionTransformerConfig(
+        state_dim=768,
+        act_dim=4672,
+    )
+    model = DecisionTransformerModel(conf)
+    yield model
---
+++ tests/utils/test_translate.py
@@ -0,0 +1,42 @@
+"""
+Test translate.
+"""
+
+import chess
+import torch
+
+import src.utils.translate as translate
+from src.models.decision_transformer import DecisionTransformerConfig, DecisionTransformerModel
+
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+torch.set_default_device(DEVICE)
+
+
+class TestEncodeSeq:
+    def test_simple_seq(self, simple_seq):
+        move_indices, _, _ = translate.encode_seq(simple_seq)
+        assert len(move_indices) == 12
+        assert move_indices[0] == chess.D2 + 64 * chess.D4  # d2 -> d4
+
+
+class TestInputFormat:
+    def test_simple_seq(self, simple_seq, default_64_model):
+        move_indices, board_tensors, end_rewards = translate.encode_seq(
+            simple_seq,
+            translate.board_to_64tensor,
+        )
+        input_dict = translate.input_format(
+            move_indices,
+            board_tensors,
+            end_rewards,
+            act_dim=4672,
+            state_dim=64,
+            device=DEVICE,
+            discount=0.99,
+            window_size=10,
+            generator=None,
+            return_dict=True,
+        )
+        assert input_dict["actions"].shape == (1, 10, 4672)
+        _ = default_64_model(**input_dict)
